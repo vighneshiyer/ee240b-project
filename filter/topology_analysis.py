db2722c53248e64978f8e81c8462f6952f6e834a
harrisonliew <harrisonliew@gmail.com>
2019-03-14 23:45:36 -0700

    topology_analysis now runs with 2 sallen-key cascaded

--- filter/topology_analysis.py
+++ filter/topology_analysis.py
@@ -6,13 +6,14 @@ from sympy.abc import f
 from sympy import I
 from scipy.constants import k
 import scipy, scipy.interpolate
+from dataclasses import dataclass
 from enum import Enum
-from filter.specs import LPFSpec, AngularFreq
+from typing import Optional, List
 from joblib import Memory
+from filter.specs import LPFSpec, OrdFreq
 
 cachedir = './cache'
 memory = Memory(cachedir, verbose=1)
-from filter.specs import LPFSpec, OrdFreq
 
 sp.init_printing()
 
@@ -28,42 +29,107 @@ ac_params = {
     'pts': 100
 }
 
-class FilterTopology(Enum):
+
+class FT(Enum):  # filter topology
     SK = 'sallen-key'
     MFB = 'multiple-feedback'
 
+
+@dataclass(frozen=True)
+class SallenKeySpec:
+    r1: float
+    r2: float
+    c1: float
+    c2: float
+    e1: float
+    ro: Optional[float] = None  # output resistance of opamp
+    e2: Optional[float] = None  # gain for GBW
+    rp: Optional[float] = None  # for GBW
+    cp: Optional[float] = None  # for GBW
+
+
+def build_lpf(cascade: List[FT], fspecs: List, ro=False, gbw=False):
+    """
+    :param cascade: list of filter topologies in the cascade
+    :param fspecs: list of filter specs
+    :param ro: consider ro?
+    :param gbw: consider GBW?
+    :return: filter, dict of subs for symbolic expressions, list noise sources
+    """
+    subs_dict = {}
+    noise_srcs = []
+    filt = ahkab.Circuit('LPF')
+    for i, t in enumerate(cascade):
+        filt, s, n = attach_stage(filt, t, fspecs[i], len(cascade), i, ro, gbw)
+        subs_dict.update(s)
+        noise_srcs = noise_srcs + n
+    print(filt)
+    print(subs_dict)
+    print(noise_srcs)
+    return filt, subs_dict, noise_srcs
+
+
 @memory.cache
-def build_sk2(d, ro = False, gbw = None):
+def attach_stage(c: ahkab.Circuit, topology: FT, fspec, stages: int, pos: int, ro=False, gbw=False):
     """
-    :param d: design_dict of all components & values (must be complete!)
-    :param ro: True if ro should be taken into account
-    :param gbw: value of finite GBW
-    :return: ahkab circuit for Sallen-Key filter of order 2.
+    :param c: circuit to append to
+    :param topology: filter topology to add
+    :param fspec: components specs
+    :param stages: total # of stages (to get in/out node correct)
+    :param pos: position in cascade
+    :param ro: consider ro?
+    :param gbw: consider GBW?
+    :return: filter, dict of subs for symbolic expressions, list noise sources
     """
-    sk2 = ahkab.Circuit('2nd Order Sallen-Key LPF')
-    sk2.add_capacitor('CL', 'out', sk2.gnd, nonideal_dict['Cload']) #load
-
-    sk2.add_resistor('R1', 'in', 'n1', d['R1'])
-    sk2.add_isource('INR1', 'in', 'n1', dc_value=0, ac_value=0) #R1 noise current
-    sk2.add_resistor('R2', 'n1', 'n2', d['R2'])
-    sk2.add_isource('INR2', 'n1', 'n2', dc_value=0, ac_value=0) #R2 noise current
-    sk2.add_capacitor('C1', 'n1', 'out', d['C1'])
-    sk2.add_capacitor('C2', 'n2', sk2.gnd, d['C2'])
-
-    #if gbw is not None:
-
-    if ro:
-        sk2.add_vcvs('E1', 'ne', sk2.gnd, 'n2', 'out', d['E1'])
-        sk2.add_resistor('RO', 'out', 'ne', d['RO']) #DANGER: this blows up the solution
-        sk2.add_isource('INE1', 'ne', sk2.gnd, dc_value=0, ac_value=0) #op-amp total output noise current
+    subs_dict = {}
+    noise_srcs = []
+    p = str(pos)
+    if pos == 0:
+        in_node = 'in'
+        c.add_vsource('V1', in_node, c.gnd, dc_value=0, ac_value=1)  # source
     else:
-        sk2.add_vcvs('E1', 'out', sk2.gnd, 'n2', 'out', d['E1'])
-        sk2.add_isource('INE1', 'out', sk2.gnd, dc_value=0, ac_value=0) #op-amp total output noise current
+        in_node = 'int_'+str(pos-1)
+    if pos == stages-1:
+        out_node = 'out'
+        c.add_capacitor('CL', out_node, c.gnd, nonideal_dict['Cload'])  # load
+        subs_dict['CL'] = nonideal_dict['Cload']
+    else:
+        out_node = 'int_'+str(pos)
 
-    sk2.add_vsource('V1', 'in', sk2.gnd, dc_value=0, ac_value=1)
+    if topology == FT.SK:
+        if not isinstance(fspec, SallenKeySpec):
+            raise ValueError("Wrong specs given for Sallen-Key Filter!")
 
-    print(sk2)
-    return sk2
+        # Add components
+        c.add_resistor('R1_'+p, in_node, 'n1_'+p, fspec.r1)
+        subs_dict['R1_'+p] = fspec.r1
+        c.add_isource('INR1_'+p, in_node, 'n1_'+p, dc_value=0, ac_value=0)
+        noise_srcs.append('INR1_'+p)
+        c.add_resistor('R2_'+p, 'n1_'+p, 'n2_'+p, fspec.r2)
+        subs_dict['R2_'+p] = fspec.r2
+        c.add_isource('INR2_'+p, 'n1_'+p, 'n2_'+p, dc_value=0, ac_value=0)
+        noise_srcs.append('INR2_'+p)
+        c.add_capacitor('C1_'+p, 'n1_'+p, out_node, fspec.c1)
+        subs_dict['C1_'+p] = fspec.c1
+        c.add_capacitor('C2_'+p, 'n2_'+p, c.gnd, fspec.c2)
+        subs_dict['C2_'+p] = fspec.c2
+
+        #if gbw:
+
+        if ro:
+            c.add_vcvs('E1_'+p, 'ne_'+p, c.gnd, 'n2_'+p, out_node, fspec.e1)
+            subs_dict['E1_'+p] = fspec.e1
+            c.add_resistor('RO_'+p, out_node, 'ne_'+p, fspec.ro)
+            subs_dict['RO_'+p] = fspec.ro
+            c.add_isource('INE1_'+p, 'ne_'+p, c.gnd, dc_value=0, ac_value=0)
+        else:
+            c.add_vcvs('E1_'+p, out_node, c.gnd, 'n2_'+p, out_node, fspec.e1)
+            subs_dict['E1_'+p] = fspec.e1
+            c.add_isource('INE1_'+p, out_node, c.gnd, dc_value=0, ac_value=0)
+        noise_srcs.append('INE1_'+p)
+
+    return c, subs_dict, noise_srcs
+
 
 def run_ac(circuit):
     """
@@ -74,11 +140,13 @@ def run_ac(circuit):
     aca = ahkab.new_ac(ac_params['start'], ac_params['stop'], ac_params['pts'])
     return ahkab.run(circuit, [opa, aca])['ac']
 
+
 @memory.cache
-def run_sym(circuit, source, print_tf = False):
+def run_sym(circuit, source, print_tf=False):
     """
     :param circuit: ahkab circuit
     :param source: name of the source for analysis
+    :param print_tf: print transfer function?
     :return: results for symbolic analysis
     """
     if not isinstance(source, str):
@@ -101,35 +169,40 @@ def run_sym(circuit, source, print_tf = False):
 
     return tfs
 
-def subs_syms(tf, d):
+
+def subs_syms(tf, subs):
     """
     :param tf: transfer function
-    :param d: design dictionary of values for R's & C's
-    :return: dictionary with values to substitute for dictionary
+    :param subs: dict of all possible values to substitute
+    :return: dict of only values to substitue for free_symbols
     """
     subs_dict = {}
     for sym in tf['gain'].free_symbols:
         if str(sym) == 's':
             subs_dict[sym] = I*2*np.pi*f
         else:
-            subs_dict[sym] = d[str(sym)]
+            subs_dict[sym] = subs[str(sym)]
 
     return subs_dict
 
-def check_specs(rac, tf, d, spec: LPFSpec):
+
+def check_specs(circuit, rac, tf, spec: LPFSpec, subs, noise_srcs):
     """
+    :param circuit: ahkab circuit
     :param rac: AC analysis result
     :param tf: transfer function w/ gain, poles, zeros
-    :param d: design dictionary of values for R's & C's
-    :param specs: target specs dictionary
+    :param spec: target specs
+    :param subs: dictionary of component value substitutions
+    :param noise_srcs: list of noise sources
     :return:
     """
     design_pass = True
 
     # Substitute design variables into transfer function
-    subs_dict = subs_syms(tf, d)
+    subs_dict = subs_syms(tf, subs)
 
     hs = sp.lambdify(f, tf['gain'].subs(subs_dict))
+    print(hs)
 
     # Print poles/zeros
     for i, z in enumerate(tf['zeros']):
@@ -167,20 +240,8 @@ def check_specs(rac, tf, d, spec: LPFSpec):
 
     # Gain ripples
 
-    return design_pass
-
-def check_dyn_range(circuit, srcs, tf, d, spec: LPFSpec):
-    """
-    :param circuit: ahkab circuit
-    :param srcs: List of all noise sources (strings)
-    :param tf: Overall filter transfer function
-    :param d: Design dict
-    :param specs: Specs dict
-    :return: total integrated noise in passband
-    """
-    design_pass = True
+    # Dynamic range
     vi2 = 0
-
     print("Noise simulation temp: {} K".format(ahkab.constants.Tref))
     kT4 = 4*k*ahkab.constants.Tref
 
@@ -188,16 +249,15 @@ def check_dyn_range(circuit, srcs, tf, d, spec: LPFSpec):
     gamma = 2/3
     gm = 0.005
 
-    for s in srcs:
+    # Integrate input-referred noise power using symbolic analysis
+    for s in noise_srcs:
         if s.startswith('INR'):
-            in2 = kT4/d[s[2:]]
+            in2 = kT4/subs[s[2:]]
         elif s.startswith('INE'):
             in2 = kT4*gamma*gm
-
         tfn = run_sym(circuit, s)
-        subs_dict = subs_syms(tfn, d)
-
-        vi2 += sp.integrate(sp.Abs(((tfn['gain']/tf['gain']).subs(subs_dict))) * in2, (f, 1, spec.passband_corner.f()))
+        vi2 += sp.integrate(sp.Abs(((tfn['gain']/tf['gain']).subs(subs_syms(tfn, subs)))) * in2,
+                            (f, 1, spec.passband_corner.f()))
 
     print("Total input referred noise power: {} V^2".format(vi2))
 
@@ -214,21 +274,6 @@ def check_dyn_range(circuit, srcs, tf, d, spec: LPFSpec):
 
     return design_pass
 
-Rbase = 900
-m = 1.5
-Cbase = 9e-12
-n = 1.5
-design_dict = { #keys must match the instance names of each component in design
-    'R1': Rbase*m,
-    'R2': Rbase/m,
-    'C1': Cbase*n,
-    'C2': Cbase/n,
-    'CL': nonideal_dict['Cload'],
-    'E1': nonideal_dict['Av'],
-    'E2': nonideal_dict['Av'],
-    'RO': nonideal_dict['RO']
-}
-
 spec = LPFSpec(
     passband_corner=OrdFreq(20e6).w(),
     stopband_corner=OrdFreq(200e6).w(),
@@ -238,8 +283,20 @@ spec = LPFSpec(
     dynamic_range=50
 )
 
-lpf = build_sk2(design_dict)
+Rbase = 900
+m = 1.5
+Cbase = 9e-12
+n = 1.5
+sk0spec = SallenKeySpec(
+    r1=Rbase*m,
+    r2=Rbase/m,
+    c1=Cbase*n,
+    c2=Cbase/n,
+    e1=nonideal_dict['Av'],
+    ro=nonideal_dict['RO']
+)
+
+lpf, subs, nsrcs = build_lpf([FT.SK, FT.SK], [sk0spec, sk0spec])
 rac = run_ac(lpf)
 tf = run_sym(lpf, 'V1', True)
-check_specs(rac, tf, design_dict, spec)
-check_dyn_range(lpf, ['INR1', 'INR2', 'INE1'], tf, design_dict, spec)
+check_specs(lpf, rac, tf, spec, subs, nsrcs)
